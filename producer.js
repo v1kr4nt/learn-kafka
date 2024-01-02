1693161b3d34d24a8bcf3195f31de02f1d8de131
vikrant <notvikrant80@gmail.com>
2024-01-02 10:28:48 +0530

    no message

--- producer.js
+++ producer.js
@@ -1,4 +1,10 @@
 const { kafka } = require("./client");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 async function init() {
   const producer = kafka.producer();
@@ -6,17 +12,25 @@ async function init() {
   await producer.connect();
   console.log("Producer connected successfully");
 
-  await producer.send({
-    topic: "rider-updates",
-    messages: [
-      {
-        partition: 0,
-        key: "location-update",
-        value: JSON.stringify({ name: "Tony Stark", loc: "SOUTH" }),
-      },
-    ],
+  rl.setPrompt("> ");
+  rl.prompt();
+
+  rl.on("line", async function (line) {
+    const [riderName, location] = line.split(" ");
+
+    await producer.send({
+      topic: "rider-updates",
+      messages: [
+        {
+          partition: location.toLowerCase() === "north" ? 0 : 1,
+          key: "location-update",
+          value: JSON.stringify({ name: riderName, loc: location }),
+        },
+      ],
+    });
+  }).on("close", async () => {
+    await producer.disconnect();
   });
-  await producer.disconnect();
 }
 
 init();
